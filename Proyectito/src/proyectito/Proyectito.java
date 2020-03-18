2a3804903d2a05dc9af48a83f663bc0f426b802b
Jorge <jjimenez17@alumnos.utalca.cl>
2020-03-18 13:57:21 -0300

    Bruh bruh bruh

--- Proyectito/src/proyectito/Proyectito.java
+++ Proyectito/src/proyectito/Proyectito.java
@@ -19,6 +19,9 @@ public class Proyectito {
         System.out.println("Hola mundito");
         System.out.println("Bruh");
         System.out.println("Hola mundito aweonaito culiaito");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("bruh");
+        }
     }
     
 }
